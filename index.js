24fcea22ab7fd1593415b3d6fda7ae716c3ef08f
Joel Goroso <yoelgoroso@gmail.com>
2022-04-03 21:42:14 -0300

    entendiendo el algoritmo pt2

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const buttons = document.querySelectorAll(".card-buttons button");
 const sections = document.querySelectorAll(".card-section");
 const card = document.querySelector(".card");
+const header = document.querySelector(".card-header");
+const text = document.querySelector(".name");
+const img = document.querySelector(".card-avatar");
 
 const handleButtonClick = (e) => {
-  //El evento click obtiene el atributo que se encuentre en data-section
   const targetSection = e.target.getAttribute("data-section");
 
   const section = document.querySelector(targetSection);
 
-  // targetSection !== "#about"
-  //   ? card.classList.add("is-active")
-  //   : card.classList.remove("is-active");
+  targetSection !== "#about"
+    ? card.classList.add("is-active")
+    : card.classList.remove("is-active");
 
-  //Setea un valor especifico en el atributo data-state y lo saca de targetSection
   card.setAttribute("data-state", targetSection);
-
-  //CREO que recorre los elementos del dom que estan llamados por el queryselectorAll y luego les remueve la clase is-active
+  header.setAttribute("data-state", targetSection);
+  text.setAttribute("data-state", targetSection);
+  img.setAttribute("data-state", targetSection);
   sections.forEach((s) => s.classList.remove("is-active"));
   buttons.forEach((b) => b.classList.remove("is-active"));
 
-  // y aca? XD
+  // ?
+  e.target.classList.add("is-active");
 
   section.classList.add("is-active");
 };
